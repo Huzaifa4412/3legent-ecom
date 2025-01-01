8be885c6765b2436e83af4314e83d93f00e2335d
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-02 00:25:10 +0500

    update

--- src/sanity/schemaTypes/featureProducts.ts
+++ src/sanity/schemaTypes/featureProducts.ts
@@ -1,71 +1,74 @@
+import { defineField, defineType } from 'sanity'
+import { NumberRule } from '@sanity/types';
 
 
-
-
-type RuleType = {
-    min: (value: number) => RuleType;
-    max: (value: number) => RuleType;
-};
-const featureProducts = {
+const featureProducts = defineType({
     name: 'featureProducts',
     title: 'Feature Products ',
     type: 'document',
     fields: [
-        {
-            name: 'name',
-            title: 'Name',
-            type: 'string',
-        },
+        defineField(
+            {
+                name: 'name',
+                title: 'Name',
+                type: 'string',
+            },
+        ),
 
-        {
+        defineField({
             name: 'discountedPrice',
             title: 'Discounted Price',
             type: 'number',
-        },
+        }),
 
-        {
-            name: 'actualPrice',
-            title: 'Actual Price',
-            type: 'number',
-        },
-        {
-            name: 'productImage',
-            title: 'Product Image',
-            type: 'image',
-            options: {
-                hotspot: true, // Allows cropping in the Sanity Studio
-            },
-        },
-        {
-            name: "slug",
-            title: "Url-Slug",
-            type: "slug",
-            options: {
-                source: 'name',
-                maxLength: 200, // will be ignored if slugify is set
-                slugify: input => input
-                    .toLowerCase()
-                    .replace(/\s+/g, '-')
-                    .slice(0, 200)
-            },
-            validation: Rule => Rule.required()
-        },
-        {
-            name: 'isOnSale',
-            title: 'Is On sale',
-            type: 'boolean', // Checkbox for marking the product as featured
-            description: 'Mark this product as if it is on sale',
-        },
-        {
+        defineField(
+            {
+                name: 'actualPrice',
+                title: 'Actual Price',
+                type: 'number',
+            }
+        ),
+        defineField(
+            {
+                name: 'productImage',
+                title: 'Product Image',
+                type: 'image',
+                options: {
+                    hotspot: true, // Allows cropping in the Sanity Studio
+                },
+            }
+        ),
+        defineField(
+            {
+                name: "slug",
+                title: "Url-Slug",
+                type: "slug",
+                options: {
+                    source: 'name',
+                    maxLength: 98, // will be ignored if slugify is set
+                },
+                validation: Rule => Rule.required()
+            }
+        ),
+        defineField(
+            {
+                name: 'isOnSale',
+                title: 'Is On sale',
+                type: 'boolean', // Checkbox for marking the product as featured
+                description: 'Mark this product as if it is on sale',
+            }
+        ),
+
+        defineField({
             name: 'discountPercent',
             title: 'Discount Percentage',
             type: 'number',
             description: 'Discount percentage for the sale.',
             hidden: ({ parent }: { parent: { isOnSale: boolean } }) => !parent?.isOnSale, // Show only if the product is on sale
-            validation: (Rule: RuleType) => Rule.min(0).max(100), // Ensure valid discount percentage
-        },
+            validation: (rule: NumberRule) => rule.min(0).max(100), // Ensure valid discount percentage
+        }),
     ],
-};
+})
 
 export default featureProducts;
 
